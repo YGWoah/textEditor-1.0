526ea435254d018c04afd5844a3f0b4f14986357
Hamza El Massoudi <hmz.elm@outlook.sa>
2023-10-05 12:06:33 +0100

    Fix : separating Router implementation and rendering app Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import './App.css';
+import React from "react";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/Error";
+import Editor from "./pages/Editor";
+import NotFound from "./pages/NotFound";
 
-function App() {
-  return <>{/* <Editor /> */}</>;
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Editor />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+const App = () => {
+  return (
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,5 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ErrorPage from "./pages/Error";
-import Editor from "./pages/Editor";
-import NotFound from "./pages/NotFound";
+import App from "./App";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Editor />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-    errorElement: <ErrorPage />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
